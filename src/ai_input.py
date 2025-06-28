82b714fa0b3fa10d077e54605133341c23b34aec
Turtlegamer_ <popperex9@gmail.com>
2025-06-28 15:50:50 +0200

    Add AI input automation with YOLO and ADB integration
    Introduces ai_input.py, which uses YOLO for object detection on a live screen capture, integrates with ADB to automate input on a connected device, and interacts with a gadget when a specific class is detected. The script captures the 'scrcpy' window, processes frames, and simulates taps based on detection results.

---
+++ src/ai_input.py
@@ -0,0 +1,70 @@
+import mss
+import numpy as np
+import cv2
+from ultralytics import YOLO
+import win32gui
+from input.gadget import clickgadget
+from ppadb.client import Client as AdbClient
+
+
+model = YOLO('src/ai model/best.pt')
+
+# Connect to ADB
+client = AdbClient(host="127.0.0.1", port=5037)
+devices = client.devices()
+if len(devices) == 0:
+    print("No devices connected.")
+    exit()
+device = devices[0]
+print(f"Connected to {device.serial}")
+
+#getting windows size and stuff
+def get_window_rect(window_name):
+    windows = []
+    def enum_windows(hwnd, lParam):
+        if window_name.lower() in win32gui.GetWindowText(hwnd).lower():
+            windows.append(hwnd)
+    win32gui.EnumWindows(enum_windows, None)
+    if windows:
+        return win32gui.GetWindowRect(windows[0])
+    else:
+        raise Exception(f"Window '{window_name}' not found.")
+
+window_name = "scrcpy" 
+left, top, right, bottom = get_window_rect(window_name)
+width = right - left
+height = bottom - top
+
+monitor = {
+    "top": top,
+    "left": left,
+    "width": width,
+    "height": height
+}
+
+#main loop
+with mss.mss() as sct:
+    while True:
+        sct_img = sct.grab(monitor)
+        img = np.array(sct_img)
+        frame = cv2.cvtColor(img, cv2.COLOR_RGBA2BGR)
+
+        results = model(frame)
+        result = results[0]
+
+        detected_classes = [model.names[int(box.cls.cpu().numpy()[0])] for box in result.boxes]
+        detected_classes = list(set(detected_classes))
+
+        if 'gadget_charged' in detected_classes:
+            #clicks gadget
+            x_gadget, y_gadget = clickgadget()
+            device.shell(f"input tap {x_gadget} {y_gadget} ")
+            
+
+        annotated_frame = result.plot()
+        cv2.imshow("YOLOv8 scrcpy Live Detection", annotated_frame)
+
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+
+cv2.destroyAllWindows()
